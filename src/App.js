e00bb1ba785d2aedf9de8675e5880b5e8638c398
rahul975337 <tilwani03@gmail.com>
2021-04-21 22:39:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import alanBtn from "@alan-ai/alan-sdk-web";
+import React, { useEffect } from "react";
+import "./App.css";
+
+const alanKey =
+  "63e03acfa4e19c102b0ffa1fd91b4a6d2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command }) => {
+        if (command === "testCommand") alert("This code was executed");
+      },
+    });
+  }, []);
+  return <div className="App">Alan AI news App</div>;
 }
 
 export default App;
